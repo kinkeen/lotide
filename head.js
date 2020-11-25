ce478add13562d41a99bcd0087bb5eceff410510
kineret.braslavsky <kineret.braslavsky@gmail.com>
2020-11-25 01:08:41 +0000

    change the code without using javascript functions

--- head.js
+++ head.js
@@ -1,16 +1,24 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log('Assertion Passed:' + actual + "===" + expected);
-    } else {
-      console.log('Assertion Failed:' + actual + "!==" + expected);
-    }
-    return assertEqual;
-  };
+  if (actual === expected) {
+    console.log('Assertion Passed:' + actual + "===" + expected);
+  } else {
+    console.log('Assertion Failed:' + actual + "!==" + expected);
+  }
+  return assertEqual;
+};
 
 const head = function(arr) {
- return arr.pop();
-}
+  if (arr.length === 0) {
+    return undefined;
+  } else {
+    return arr[0];
+  }
+  //let firstItem = arr.filter(x => typeof x !== undefined).shift();
+  //return firstItem;
+};
 
-assertEqual(head([5,6,7,9,40]), 5);
+assertEqual(head([1]), 5);
+assertEqual(head([]), 5);
+assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
