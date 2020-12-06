1a04d70f9aa646902cdc160a7c3f757b831cd58c
kineret.braslavsky <kineret.braslavsky@gmail.com>
2020-12-06 21:52:21 +0000

    clean up and remove all the comment and the tests. implement unit test tets/headTest.js for this function so don't need anymore in it

--- head.js
+++ head.js
@@ -1,12 +1,4 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log('Assertion Passed:' + actual + "===" + expected);
-  } else {
-    console.log('Assertion Failed:' + actual + "!==" + expected);
-  }
-  return assertEqual;
-};
 
 const head = function(arr) {
   if (arr.length === 0) {
@@ -14,11 +6,6 @@ const head = function(arr) {
   } else {
     return arr[0];
   }
-  //let firstItem = arr.filter(x => typeof x !== undefined).shift();
-  //return firstItem;
 };
 
-assertEqual(head([1]), 5);
-assertEqual(head([]), 5);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
