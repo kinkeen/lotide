74005b5d3ce23975055b4cb42166860156c4ff76
kineret.braslavsky <kineret.braslavsky@gmail.com>
2021-01-05 01:14:36 +0000

    added coments

--- head.js
+++ head.js
@@ -1,6 +1,11 @@
 // FUNCTION IMPLEMENTATION
 //const assertEqual = require('./assertEqual');
 
+
+// function that retrieve the first element from the array
+//An array with only one element yield that one element as its head
+//An empty array yield undefined as its head
+
 const head = function(arr) {
   if (arr.length === 0) {
     return undefined;
