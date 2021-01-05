54acd87f1fa1dc2a71f42b23675a30f297cd8c6b
kineret.braslavsky <kineret.braslavsky@gmail.com>
2021-01-05 23:28:38 +0000

    commited  index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const map = require('./map');
+module.exports = {
+  head,
+  tail,
+  middle,
+  without,
+  takeUntil,
+  map
+};
